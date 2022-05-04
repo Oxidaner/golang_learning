f52b15f44ee2c101946ac79f2da728e81ad5ef43
Oxidaner <18622412361@163.com>
2022-05-05 00:51:30 +0800

    learn

---
+++ learn_code/fundamental/Slice/Slice06 struct Array&Slice成员/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	d := [5]struct {
+		x int
+	}{}
+
+	s := d[:]
+
+	d[1].x = 10
+	s[2].x = 20
+
+	fmt.Println(d)
+	fmt.Printf("%p, %p\n", &d, &d[0])
+
+}
--- learn_code/fundamental/underline/underlin01/main.go
+++ learn_code/fundamental/underline/underlin01/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"learn_code/fundamental/underline/underline01/hello"
+	_ "learn_code/fundamental/underline/underlin01/hello"
 )
 
 func main() {
-	hello.Print()
+	//hello.Print()
 	//编译报错：./main.go:6:5: undefined: hello
 }
