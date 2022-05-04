5d2bcee61b3f71a3b189ebdb3398ffc6456172a7
Oxidaner <18622412361@163.com>
2022-05-05 00:30:30 +0800

    learn

---
+++ learn_code/fundamental/underline/underline01/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"learn_code/fundamental/underline/underline01/hello"
+)
+
+func main() {
+	hello.Print()
+	//编译报错：./main.go:6:5: undefined: hello
+}
---
+++ learn_code/go.mod
@@ -0,0 +1,3 @@
+module learn_code
+
+go 1.18
